b50eb5f13f665b66fdce4b60addd83a78ff1013e
glitchcore555 <kovuwallen@gmail.com>
2026-02-02 19:04:24 -0800

    added Alcohol item and Creative mode tab

--- src/main/java/net/glitchcore/modernwarfare/item/drinks/Alcohol.java
+++ src/main/java/net/glitchcore/modernwarfare/item/drinks/Alcohol.java
@@ -33,7 +33,7 @@ public class Alcohol extends Item {
 
         if (!level.isClientSide) {
             entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION,20*30,10));
-            entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,20* 30,10));
+            entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,20* 30,1));
             entity.addEffect(new MobEffectInstance(MobEffects.POISON,20* 30,1));
         }
 
